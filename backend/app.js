005cc39125c6d19fb52855c214c1d65e89d11774
swapnil ramteke <97676520+swapnilpreet@users.noreply.github.com>
2026-04-27 20:26:47 +0530

    added

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,11 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://your-frontend-url.vercel.app"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
--- backend/config/socket.js
+++ backend/config/socket.js
@@ -2,19 +2,21 @@ let io;
 
 const initSocket = (server) => {
   const socketIO = require("socket.io");
-
   io = socketIO(server, {
-    cors: { origin: "*" }
+    cors: {
+      origin: "*", //fronend url
+      methods: ["GET", "POST"]
+    }
   });
-
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
-
     socket.on("join_project", (projectId) => {
       socket.join(projectId);
     });
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
   });
-
   return io;
 };
 
