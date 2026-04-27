943c2f6212818fbf2f998836a5f0096639cab06f
swapnil ramteke <97676520+swapnilpreet@users.noreply.github.com>
2026-04-27 23:44:06 +0530

    added

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://your-frontend-url.vercel.app"],
+  origin: ["http://localhost:5173", "https://collaborative-workflow-orchestratio.vercel.app"],
   credentials: true
 }));
 
